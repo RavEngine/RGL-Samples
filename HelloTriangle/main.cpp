8850a4c73dddf30a1e9217d032ed852fcb5b7610
Ravbug <ravbuganimations@gmail.com>
2022-12-26 17:55:35 -0800

    Update for API changes

--- HelloTriangle/main.cpp
+++ HelloTriangle/main.cpp
@@ -1,9 +1,14 @@
 #include <RGL/RGL.hpp>
 #include "Common/App.hpp"
 #include <RGL/Device.hpp>
+#include <RGL/Surface.hpp>
+#include <iostream>
+#include <SDL.h>
+#include <SDL_syswm.h>
 
 struct HelloWorld : public AppBase {
 	std::shared_ptr<RGL::IDevice> device;
+	std::shared_ptr<RGL::ISurface> surface;
 
 	const char* SampleName() {
 		return "HelloTriangle";
@@ -18,13 +23,40 @@ struct HelloWorld : public AppBase {
 		};
 		RGL::Init(options);
 
+		auto backendstr = RGL::APIToString(RGL::CurrentAPI());
+		std::cout << backendstr << std::endl;
+
+		// next make a device
 		device = RGL::IDevice::CreateSystemDefaultDevice();
+		std::cout << device->GetBrandString() << std::endl;
+
+		// create a surface to render to
+		// this requires some platform-specific code
+		SDL_SysWMinfo wmi;
+		SDL_VERSION(&wmi.version);
+		if (!SDL_GetWindowWMInfo(window, &wmi)) {
+			throw std::runtime_error("Cannot get native window information");
+		}
+		surface = RGL::CreateSurfaceFromPlatformHandle(
+#ifdef _WIN32
+			&wmi.info.win.window
+#else
+#error not implemented
+#endif
+		
+		);
 	}
 	void tick() final {
 
 	}
 
 	void shutdown() final {
+		// need to null these out before shutting down, otherwise validation errors will occur
+		// take care the order that these were initialized in - in general they should be uninitialized in reverse order
+		
+		surface.reset();
+		device.reset();
+
 		RGL::Shutdown();
 	}
 };
