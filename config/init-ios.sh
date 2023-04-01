e17695ef08d49f81e9b3a5098fa8d8a1371f2532
Ravbug <ravbuganimations@gmail.com>
2023-04-01 16:55:36 -0400

    Disable ARC on iOS

--- config/init-ios.sh
+++ config/init-ios.sh
@@ -2,4 +2,4 @@ mkdir ../build
 cd ../build
 mkdir ios
 cd ios
-cmake -G "Xcode" -DCMAKE_TOOLCHAIN_FILE="../../config/ios.toolchain.cmake" -DPLATFORM="OS64COMBINED" -DDEPLOYMENT_TARGET=14.0 ../..
+cmake -G "Xcode" -DCMAKE_TOOLCHAIN_FILE="../../config/ios.toolchain.cmake" -DENABLE_ARC=OFF -DPLATFORM="OS64COMBINED" -DDEPLOYMENT_TARGET=14.0 ../..
