13a6fbe08f8eb234bb6b11a596152f62be134081
Ravbug <ravbuganimations@gmail.com>
2023-05-12 22:55:07 -0700

    Add webgpu shader targets

--- config/init-web.bat
+++ config/init-web.bat
@@ -2,5 +2,5 @@
 cd ..
 mkdir build
 mkdir build\web
-cd build/web
-cmake -Wno-dev -DCMAKE_TOOLCHAIN_FILE=../../../emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake ../..
+cd build\web
+..\..\..\emsdk\upstream\emscripten\emcmake.bat cmake -Wno-dev -DCMAKE_TOOLCHAIN_FILE=../../../emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake ../..
