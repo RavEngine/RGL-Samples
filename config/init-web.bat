34191d2bdb6b9103b063d8fcd382e0392089e5ab
Ravbug <ravbug@users.noreply.github.com>
2023-09-02 16:25:24 -0400

    Update web scripts

--- config/init-web.bat
+++ config/init-web.bat
@@ -3,4 +3,4 @@ cd ..
 mkdir build
 mkdir build\web
 cd build\web
-..\..\..\emsdk\upstream\emscripten\emcmake.bat cmake -Wno-dev -DCMAKE_TOOLCHAIN_FILE=../../../emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake ../..
+emcmake cmake ..\..
