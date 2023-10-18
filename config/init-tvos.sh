46ee9dbfc0d0e6b672d8d26a1f755e7ba6ef3303
Ravbug <ravbug@users.noreply.github.com>
2023-10-18 12:28:35 -0400

    Attempt to fix iOS issues

--- config/init-tvos.sh
+++ config/init-tvos.sh
@@ -2,4 +2,4 @@ mkdir ../build
 cd ../build
 mkdir tvos
 cd tvos
-cmake -G "Xcode" -DCMAKE_TOOLCHAIN_FILE="../../config/ios.toolchain.cmake" -DPLATFORM="TVOSCOMBINED" -DDEPLOYMENT_TARGET=14.0 ../..
+cmake -G "Xcode" -DCMAKE_SYSTEM_NAME="tvOS" ../..
